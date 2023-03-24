40063dd2f51b70fa426d4bae40db36c013757c9d
AngeiraT <thaispur04@icloud.com>
2023-03-24 14:04:51 -0400

    add readme and file

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Unlock the boxes
+    """
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
