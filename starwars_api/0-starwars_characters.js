5f11b83f96179905a8e3e3d25fd7d091d4fec9ae
AngeiraT <4985@holbertonstudents.com>
2023-07-14 22:14:51 -0400

    semistandar validation

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,20 +1,24 @@
 #!/usr/bin/node
 
-
 const request = require('request');
-const filmNum = process.argv[2] + '/';
-const filmURL = 'https://swapi-api.hbtn.io/api/films/';
-
-request(filmURL + filmNum, async function (err, res, body) {
-  if (err) return console.error(err);
-  const charURLList = JSON.parse(body).characters;
-  for (const charURL of charURLList) {
-    await new Promise(function (resolve, reject) {
-      request(charURL, function (err, res, body) {
-        if (err) return console.error(err);
-        console.log(JSON.parse(body).name);
-        resolve();
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, async function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character in characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(characters[character], (err, res, html) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
       });
-    });
+      console.log(res);
+    }
   }
 });
+
